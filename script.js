96100650438e459c1bff6cad24dbfb10cf93138c
Welington Argolo Guilhardi <welingtonpitbull123@gmail.com>
2020-11-14 18:25:08 -0300

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+var hamburger=document.querySelector(".menu-hamburger")
+hamburger.addEventListener("click", function(){
+    document.querySelector(".sidebar").classList.toggle("show-menu")
+    
+
+})
